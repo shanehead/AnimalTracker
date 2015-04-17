db1528458bbb0fe6e4dc3a93d7380687d917613c
Shane Head <shane.head@gmail.com>
2015-04-17 16:25:14 -0500

    Get the database URI set correctly.

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class DevConfig(Config):
 	if os.environ.get('DATABASE_URL') is None:
 		SQLALCHEMY_DATABASE_URI = "postgresql://localhost/animal_tracker"
 	else:
-		SQLALCHEMY_DATABASE_URI = "postgresql://localhost/animal_tracker_dev"
+		SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class TestConfig(Config):
 	TESTING = True
