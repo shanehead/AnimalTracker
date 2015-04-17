e4ad9af1ae300745d908af0ef8bfec87f4de200b
Shane Head <shane.head@gmail.com>
2015-04-17 15:21:24 -0500

    Adding requirements.txt and using environement DATABASE_URL

--- config.py
+++ config.py
@@ -5,7 +5,10 @@ class Config(object):
 	SITE_NAME = 'Animal Tracker'
 	WTF_CSRF_ENABLED = True
 	SECRET_KEY = 'q\\r8^!@uLcw/z90|y{c,6B56q2vw!mlG'
-	SQLALCHEMY_DATABASE_URI = "postgresql://localhost/animal_tracker"
+	if os.environ.get('DATABASE_URL') is None:
+		SQLALCHEMY_DATABASE_URI = "postgresql://localhost/animal_tracker"
+	else:
+		SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 	MEDIA_URL = '/uploads'
 	MEDIA_FOLDER = os.path.join(basedir, 'app' + MEDIA_URL)
 	GOOGLE_LOGIN_CLIENT_ID='128739772730-itrb3b45v3nnu99sdk5e4h8tvrkp2t4b.apps.googleusercontent.com'
@@ -16,10 +19,16 @@ class DevConfig(Config):
 	ASSETS_DEBUG = True
 	DEBUG = True
 	WTF_CSRF_ENABLED = False
-	SQLALCHEMY_DATABASE_URI = "postgresql://localhost/animal_tracker_dev"
+	if os.environ.get('DATABASE_URL') is None:
+		SQLALCHEMY_DATABASE_URI = "postgresql://localhost/animal_tracker"
+	else:
+		SQLALCHEMY_DATABASE_URI = "postgresql://localhost/animal_tracker_dev"
 
 
 class TestConfig(Config):
 	TESTING = True
 	WTF_CSRF_ENABLED = False
-	SQLALCHEMY_DATABASE_URI = "postgresql://localhost/animal_tracker_test"
+	if os.environ.get('DATABASE_URL') is None:
+		SQLALCHEMY_DATABASE_URI = "postgresql://localhost/animal_tracker"
+	else:
+		SQLALCHEMY_DATABASE_URI = "postgresql://localhost/animal_tracker_test"
